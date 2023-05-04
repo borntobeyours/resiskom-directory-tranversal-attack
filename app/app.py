af72a89caf00b878fd9ff6c8a61e70556eb43e00
Borntobeyours <borntobeyours@gmail.com>
2023-05-04 21:06:47 +0700

    save

--- app/app.py
+++ app/app.py
@@ -12,8 +12,7 @@ def get_file():
     file_path = request.args.get('path')
     if file_path:
         try:
-            parent_dir = os.path.dirname(os.path.abspath(__file__))
-            return send_from_directory(os.path.join(parent_dir, 'static'), file_path)
+            return send_from_directory('static', file_path)
         except:
             abort(404)
     else:
